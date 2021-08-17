85c35de60a24643189405ae45653346c8d7a81cb
lp6m <lp6m2014@gmail.com>
2021-08-17 22:58:52 +0900

    modify layout:

--- app/tflite_yolov5_test/app/src/main/java/com/example/tflite_yolov5_test/ui/dashboard/DashboardFragment.java
+++ app/tflite_yolov5_test/app/src/main/java/com/example/tflite_yolov5_test/ui/dashboard/DashboardFragment.java
@@ -28,13 +28,6 @@ public class DashboardFragment extends Fragment {
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 
